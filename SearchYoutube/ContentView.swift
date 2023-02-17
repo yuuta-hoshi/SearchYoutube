99a4d2c43cd1bac06b1572ff4fadb2c59000053b
yuuta-hoshi <h_y0608@icloud.com>
2023-02-17 22:20:51 +0900

    Initial Commit

---
+++ SearchYoutube/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SearchYoutube
+//
+//  Created by 星優大 on 2023/02/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchYoutube/SearchYoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchYoutubeApp.swift
+//  SearchYoutube
+//
+//  Created by 星優大 on 2023/02/17.
+//
+
+import SwiftUI
+
+@main
+struct SearchYoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
